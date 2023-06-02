800444e2d93df117fbbbfee2a8fb7eaa392d523a
Nuklles <103897836+Nuklles@users.noreply.github.com>
2023-06-02 10:52:23 -0300

    Update menu_usuario.php

--- Canil/menu_usuario.php
+++ Canil/menu_usuario.php
@@ -1,79 +1,63 @@
-<?php
-session_start();
-include('validar_usuario.php');
-include('conexao.php');
-$email = $_SESSION['email'];
-
-$select = "SELECT email FROM login WHERE email = '$email'";
-$query = mysqli_query($conexao, $select);
-$dado = mysqli_fetch_row($query);
-echo $dado[0];
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Menu</title>
-	<style>
-		body {
-			font-family: Arial, sans-serif;
-			background-color: #f2f2f2;
-		}
-		h1 {
-			text-align: center;
-			font-size: 36px;
-			color: #333;
-			margin-top: 30px;
-		}
-		ul {
-			list-style: none;
-			padding: 0;
-			margin: 50px auto;
-			display: table;
-		}
-		li {
-			display: table-row;
-			margin-bottom: 20px;
-			background-color: #fff;
-			border-radius: 5px;
-			box-shadow: 0 2px 5px rgba(0,0,0,0.1);
-			transition: all 0.2s ease-in-out;
-		}
-		li:hover {
-			transform: scale(1.05);
-			box-shadow: 0 5px 15px rgba(0,0,0,0.2);
-		}
-		a {
-			display: table-cell;
-			vertical-align: middle;
-			padding: 20px;
-			color: #333;
-			text-decoration: none;
-			font-size: 24px;
-		}
-		a:hover {
-			color: #0077b5;
-			text-decoration: underline;
-		}
-		.logout {
-			display: block;
-			margin: 0 auto;
-			text-align: center;
-			font-size: 18px;
-			color: #999;
-			margin-top: 50px;
-		}
-		
-	</style>
+    <title>Menu</title>
+    <style>
+        
+	 body{
+       
+	     background-image: url("https://cdn.discordapp.com/attachments/968520978227408906/1114002180937109505/Yellow_and_Orange_Playful_Pet_Adoption_Promotion_Banner_1.png");
+            background-size: cover;
+            background-repeat: no-repeat;
+            font-family: Arial, sans-serif;
+            background-color: #42DAF5;
+            margin: ;
+            padding: 0;
+        }
+		.sidebar {
+			
+            background-color: #333;
+            color: #fff;
+            width: 200px;
+            height: 100vh;
+            position: fixed;
+            left: 0;
+            top: 0;
+            padding-top: 20px;
+        }
+
+        .sidebar ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .sidebar li {
+            margin-bottom: 10px;
+        }
+
+        .sidebar li a {
+            display: block;
+            padding: 10px;
+            color: #fff;
+            text-decoration: none;
+        }
+
+        
+	
+    </style>
 </head>
 <body>
-	<h1>Menu usuario</h1>
-	 <li><a href="editar_perfil.php">Editar perfil</a></li>
-	 <li><a href="gerenciador_posteres.php">Gerenciador de posteres</a></li>
-	 <li><a href="inserir_poster.php">Inserir poster</a></li>
-	 <li><a href="adotar_pet.php">Adotar pet</a></li>
-	 <li><a href="encaminhar_pet.php">Encaminhar pet para um canil</a></li>
-	<div class="logout"><a href="sair.php">Sair</a></div>
-	
+    <div class="sidebar">
+        <h1>Seja Bem Vindo</h1>
+	    <li><a href="editar_perfil.php">Editar perfil</a></li>
+        <li><a href="gerenciador_posteres.php">Gerenciador de posteres</a></li>
+        <li><a href="inserir_poster.php">Inserir poster</a></li>
+        <li><a href="adotar_pet.php">Adotar pet</a></li>
+        <li><a href="encaminhar_pet.php">Encaminhar pet para um canil</a></li>
+        <li><a href="sair.php">Sair</a></li>
+        </ul>
+    </div>
+  
 </body>
 </html>
