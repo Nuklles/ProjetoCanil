a2fe7175482ef6c33ebf1a509bfc819a0d545f85
Nuklles <103897836+Nuklles@users.noreply.github.com>
2023-06-04 13:50:25 -0300

    Update cadastrar_empresa.php

--- Canil/cadastrar_empresa.php
+++ Canil/cadastrar_empresa.php
@@ -2,22 +2,108 @@
 <html>
 
 <head>
-    <title>Cadastro de Empresa</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-2r7BR16rjGytrGcxkkK2YX1op6aJ7WUyrxm0xApfQVQhbiyWqUP3rd76T3+3YcJ3mIr2IRv4yAij3E1TDcnFWA==" crossorigin="anonymous" />
     <style>
+        .background-image {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+        background-image: url("https://cdn.discordapp.com/attachments/968520924087328771/1114624732122337350/Banner_projeto_social_adocao_de_animais_amarelo_e_azul_1.png");
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+        body {
+            background-color: #FFD84D;
+            font-family: Arial, sans-serif;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .container {
+            border-radius: 10px;
+            padding: 50px;
+            box-shadow: 0 20px 50px rgba(10, 20, 30, 0.1);
+            max-width: 400px;
+            width: 100%;
+            text-align: center;
+            background-color: #3E9AAB;
+        }
+
+        h2 {
+            font-size: 24px;
+            color: #000000;
+            margin-top: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: #FFC041;
+            margin-bottom: 20px;
+        }
+
+        label {
+            font-weight: bold;
+            color: #FFC041;
+            font-size: 14px;
+            display: block;
+            text-align: left;
+            margin-bottom: 5px;
+        }
+
+        input[type="text"],
+        input[type="email"],
+        input[type="password"],
+        input[type="checkbox"],
+        input[type="estado"],
+        input[type="cidade"],
+        input[type="cep"] {
+            width: 100%;
+            padding: 8px;
+            border: none;
+            border-radius: 4px;
+            margin-bottom: 10px;
+        }
+
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            width: 100%;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+
+        a {
+            color: #FFC041;
+            text-decoration: none;
+        }
+
         .success {
             color: green;
+            margin-top: 10px;
         }
 
         .error {
             color: red;
+            margin-top: 10px;
         }
-
-        
     </style>
 </head>
 
 <body>
-    <h2>Cadastro de Empresa</h2>
+    <div class="background-image"></div>
+    <div class="container">
+        <h2>Cadastro de Empresa</h2>
     <?php
     $successMessage = $errorMessage = "";
 
@@ -119,6 +205,7 @@
         <p class="error"><?php echo $errorMessage; ?></p>
     <?php endif; ?>
     <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+     <center>    
         <label>Nome:</label>
         <input type="text" name="nome" required><br><br>
         <label>CNPJ:</label>
@@ -137,14 +224,15 @@
         <input type="password" name="senha" required><br><br>
         <label>Confirmar Senha:</label>
         <input type="password" name="confirmar_senha" required><br><br>
+        </center>
         <input type="checkbox" name="checkbox1" value="1"> Aceito os <a href onclick="alert('não existe termos!!!!')">termos</a></a><br><br>
         <input type="submit" value="Cadastrar">
         <a href="http://"></a>
-        
-                
-
     </form>
     <br><br>
+    <center>
     <a href="cadastrar.php">Voltar</a>
+    </center>
+
 </body>
 </html>
