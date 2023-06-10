2f55bb732e9448c9eb835c9f6d7fbd51415611b0
Nuklles <103897836+Nuklles@users.noreply.github.com>
2023-06-10 00:34:23 -0300

    Update editar_poster.php

--- Canil/editar_poster.php
+++ Canil/editar_poster.php
@@ -46,16 +46,66 @@ $dados = mysqli_fetch_assoc($query);
 <html>
 <head>
   <style>
-    
+    body {
+      background-color: #f5f5f5;
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+    .container {
+      border-radius: 10px;
+      background-color: #D8BFD8;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      max-width: 400px;
+      margin: 50px auto;
+      padding: 50px;
+    }
+    h2 {
+      color: #333;
+      font-size: 24px;
+      margin-top: 0;
+      text-align: center;
+    }
+    label {
+      color: #333;
+      display: block;
+      font-weight: bold;
+      margin-top: 10px;
+    }
+    input[type="text"] {
+      width: 95%;
+      padding: 8px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      margin-top: 5px;
+    }
+    input[type="submit"] {
+      background-color: #4caf50;
+      border: none;
+      color: white;
+      padding: 10px 20px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      border-radius: 4px;
+      cursor: pointer;
+      margin-top: 10px;
+    }
+    input[type="submit"]:hover {
+      background-color: #45a049;
+    }
+    input[type="submit"][name="voltar"] {
+      background-color: #aaa;
+    }
+    input[type="submit"][name="voltar"]:hover {
+      background-color: #999;
+    }
   </style>
 </head>
 <body>
   <div class="container">
     <h2>Editar Poster</h2>
-    <?php if (!empty($mensagem)) : ?>
-      <p><?php echo $mensagem; ?></p>
-    <?php endif; ?>
-
     <form method="POST" action="">
       <input type="hidden" name="id_pets" value="<?php echo $id_pets; ?>">
 
